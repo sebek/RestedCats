91e2f47acd517d463ee27f848152624d0f93b25d
Sebastian Kemi <sebastian.kemi@rule.se>
2014-10-19 02:39:36 +0200

    Expanded on comments

--- app/Helpers/Validation.php
+++ app/Helpers/Validation.php
@@ -1,18 +1,52 @@
 <?php namespace RestedCats\Helpers;
 
+use InvalidArgumentException;
+
+/**
+ * Class Validation
+ * @package RestedCats\Helpers
+ *
+ * Run validations against specified rules.
+ * Implemented rules:
+ *  + required
+ *  + numeric
+ *  + string
+ *
+ * Format:
+ *      "values" => [
+ *          "field" => "My Field Value"
+ *      ]
+ *
+ *      "config" => [
+ *          "field" => [
+ *              "required",
+ *              "string
+ *          ]
+ *      ]
+ */
 class Validation
 {
     protected $errorMessages = [];
 
+    /**
+     * Run validation against values
+     *
+     * @param array $values
+     * @param array $config
+     * @return bool
+     */
     public function run(array $values, array $config)
     {
+        // Empty old error messages and set the validation return to true
         $this->clearErrorMessages();
         $validated = true;
 
+        // If both values and configured rules are empty, we don't care
         if (empty($values) && empty($config)) {
             return true;
         }
 
+        // Loop through rules and compare them against the values
         foreach ($config as $key => $rules) {
             foreach ($rules as $rule) {
                 if ($this->checkRule($rule, $values, $key) === false) {
@@ -24,15 +58,30 @@ class Validation
         return $validated;
     }
 
+    /**
+     * Run the rule specified or throw InvalidArgumentException
+     *
+     * @param $rule
+     * @param $values
+     * @param $key
+     * @return mixed
+     */
     public function checkRule($rule, $values, $key)
     {
         if (method_exists($this, $rule)) {
             return $this->{$rule}($values, $key);
         }
 
-        return false;
+        throw new InvalidArgumentException("{$rule} isn't a valid validation rule.");
     }
 
+    /**
+     * Check if the value is required
+     *
+     * @param $values
+     * @param $key
+     * @return bool
+     */
     public function required($values, $key)
     {
         // Check for the value
@@ -44,6 +93,13 @@ class Validation
         return false;
     }
 
+    /**
+     * Check if the value is numeric
+     *
+     * @param $values
+     * @param $key
+     * @return bool
+     */
     public function numeric($values, $key)
     {
         // We don't care about required
@@ -60,6 +116,13 @@ class Validation
         return false;
     }
 
+    /**
+     * Check if the value is a string
+     *
+     * @param $values
+     * @param $key
+     * @return bool
+     */
     public function string($values, $key)
     {
         // We don't care about required
--- app/routes.php
+++ app/routes.php
@@ -6,12 +6,20 @@ use RestedCats\Helpers\Response;
 use RestedCats\Helpers\Validation;
 use RestedCats\Repositories\CatsRepository;
 
+// Helper Classes
 $input = new Input();
 $response = new Response();
 $validation = new Validation();
+
+// Repositories
 $catsRepository = new CatsRepository();
+
+// Routing
 $route = new Router('/index.php/');
 
+/**
+ * Find cat by id
+ */
 $route->get('/cats/*', function ($id) use ($response, $catsRepository) {
     $cat = $catsRepository->find($id);
 
@@ -22,6 +30,9 @@ $route->get('/cats/*', function ($id) use ($response, $catsRepository) {
     return $response->send("Could not find cat with id {$id}", 404);
 });
 
+/**
+ * Find cat by name
+ */
 $route->get('/cats/*/byName', function ($name) use ($response, $catsRepository) {
     $cat = $catsRepository->findByName($name);
 
@@ -32,11 +43,17 @@ $route->get('/cats/*/byName', function ($name) use ($response, $catsRepository) 
     return $response->send("Could not find cat with name {$name}", 404);
 });
 
+/**
+ * Fetch and return every cat in the database
+ */
 $route->get('/cats/', function () use ($response, $catsRepository) {
     $cats = $catsRepository->all();
     return $response->send($cats);
 });
 
+/**
+ * Add a new cat to the database
+ */
 $route->post('/cats/', function () use ($input, $response, $validation, $catsRepository) {
 
     $fields = $input->all();
@@ -58,6 +75,9 @@ $route->post('/cats/', function () use ($input, $response, $validation, $catsRep
     return $response->send("Unknown error, ask you nearest cat", 500);
 });
 
+/**
+ * Update a cat
+ */
 $route->put('/cats/*', function ($id) use ($input, $response, $validation, $catsRepository) {
 
     $fields = $input->all();
@@ -79,6 +99,9 @@ $route->put('/cats/*', function ($id) use ($input, $response, $validation, $cats
     return $response->send("Could not find cat with id {$id}", 404);
 });
 
+/**
+ * Delete a cat
+ */
 $route->delete('/cats/*', function ($id) use ($response, $catsRepository) {
     if ($catsRepository->delete($id)) {
         return $response->send("Cat deleted", 200);
